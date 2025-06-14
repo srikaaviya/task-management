397449f13a64e1be7f6c37db727c2ea8487037ce
Jean <jeansalazar135@gmail.com>
2025-06-14 13:06:44 -0400

    feat(signup): add signup view, URL, and template

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
     return render(request, 'index.html')
+    
+def signup(request):
+    return render(request, 'signup.html', {
+        'form': UserCreationForm
+    })
