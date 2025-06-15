f6eba17f3294f82754b2fdd3651d3b1dafc612dc
Jean <jeansalazar135@gmail.com>
2025-06-14 21:42:20 -0400

    feat(signup): enhance signup functionality with error handling and user feedback

--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.contrib import messages
+from django.db import IntegrityError
+
 
 def home(request):
     return render(request, 'index.html')
     
 def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            try:
+                user = form.save()
+                messages.succes(request, 'User created successfully!')
+            except IntegrityError:
+                form.add_error('username', 'This username already exists')
+            except Exception as e:
+                messages.error(request, f"An error has ocurred: {e}")
+        else:
+            messages.error(request, 'Please, correct errors in the form')
+    else:
+        form = UserCreationForm()
+        
     return render(request, 'signup.html', {
-        'form': UserCreationForm
+        'form': form
     })
