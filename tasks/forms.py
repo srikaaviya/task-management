4aaab497887a222db13767e185b67037439538de
jscode-1302 <jeansalazar135@gmail.com>
2025-07-01 21:23:29 -0400

    add due date field to Task model and update forms and templates. Change time zone to New York

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,7 +1,11 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Task
 
 class TaskForm(ModelForm):
     class Meta:
         model = Task
-        fields = ['title', 'description', 'priority', 'status']
+        fields = ['title', 'description', 'priority', 'status', 'due_date']
+        widgets = {
+            'due_date': forms.DateTimeInput(attrs={'type': 'datetime-local'})
+        }
--- tasks/models.py
+++ tasks/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Task(models.Model):
     class Priority(models.TextChoices):
@@ -16,10 +17,15 @@ class Task(models.Model):
     description = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    due_date = models.DateTimeField(null=True, blank=True, verbose_name='Due Date')
     priority = models.CharField(max_length=10, choices=Priority.choices, default=Priority.LOW)
     status = models.CharField(max_length=20, choices=Status.choices, default=Status.PENDING)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __str__(self):
         return f"{self.title} by: {self.user.username}"
+    
+    @property
+    def is_overdue(self):
+        return (self.due_date and self.due_date <  timezone.now() and self.status != 'completed')
     
